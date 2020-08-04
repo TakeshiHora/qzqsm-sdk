e00f876ccf27903b42a52190c76a751fca10ee63
TakeshiHora <a1936th@aiit.ac.jp>
2020-08-05 01:26:57 +0900

    revised TestCode

--- src/test/java/jp/ac/aiit/pbl/disaster/seismicIntensity/SeismicIntensityParserTest.java
+++ src/test/java/jp/ac/aiit/pbl/disaster/seismicIntensity/SeismicIntensityParserTest.java
@@ -1,8 +1,15 @@
 package jp.ac.aiit.pbl.disaster.seismicIntensity;
 
+import jp.ac.aiit.pbl.disaster.DisasterCategory;
 import jp.ac.aiit.pbl.disaster.seismicIntensity.SeismicIntensity;
 import jp.ac.aiit.pbl.disaster.seismicIntensity.SeismicIntensityParser;
 import org.junit.Test;
+
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 
 /**
  * This Class is SeismicIntensity Parser Test.
@@ -15,9 +22,16 @@ public class SeismicIntensityParserTest {
     public void seismicIntensityParserTest() {
         SeismicIntensityParser seismicIntensityParser = new SeismicIntensityParser();
         
-        //SeismicIntensity seismicIntensity = seismicIntensityParser.parse("110001101010110111110010110001010001011010000000000000101100010001110　101　010110　001　000111　111　001011　000　101111　100　000101　100　101110　111　000010　110　011000　001　101011　011　000000　100　000000　101　100111　110　100000　010　100001　000　000010　000　000000");
-        SeismicIntensity seismicIntensity = seismicIntensityParser.parse("110001101010110111110010110001010001011010000000000000101100010001110101010110001000111111001011000101111100000101100101110111000010110011000001101011011000001100000000101100111110100000010100001000000010000000000");
-        
+        SeismicIntensity seismicIntensity = seismicIntensityParser.parse("1100011010101101100110101100010100010110100000000000001011000100011101010101100010001111110010110001011111000001011001011101110000101100110000011010110110000011000000001011001111101000000101000010000000100000000000000000000000");
         System.out.println(seismicIntensity.toString());
+    }
+    @Test
+    public void seismicIntensityDigitTest(){
+        SeismicIntensityParser seismicIntensityParser = new SeismicIntensityParser();
+        SeismicIntensity seismicIntensity = seismicIntensityParser.parse("1100011010101101100110101100010100010110100000000000001011000100011101010101100010001111110010110001011111000001011001011101110000101100110000011010110110000011000000001011001111101000000101000010000000100000000000000000000000");
+        
+        assertThat(seismicIntensity.getDisasterCategory(), is(DisasterCategory.SeismicIntensity));
+        assertThat(seismicIntensity.getRegions().size(), is(16));
+        assertThat(seismicIntensity.getOccurrenceTime(), is(LocalDateTime.of(2020,8,5,17,7)));
     }
 }
--- src/test/java/jp/ac/aiit/pbl/disaster/seismicIntensity/WarningTest.java
+++ src/test/java/jp/ac/aiit/pbl/disaster/seismicIntensity/WarningTest.java
@@ -14,31 +14,31 @@ import static org.junit.Assert.*;
 public class WarningTest {
     
     @Test
-    public void cangetSeismicIntensityLessThan4() {
+    public void inCaseOfValue1GetSeismicIntensityLessThan4() {
         assertThat(Warning.SeismicIntensityLessThan4, is(Warning.getSeismicIntensity(1)));
     }
     @Test
-    public void cangetSeismicIntensitySeismicIntensity4() {
+    public void inCaseOfValue2GetSeismicIntensity4() {
         assertThat(Warning.SeismicIntensity4, is(Warning.getSeismicIntensity(2)));
     }
     @Test
-    public void cangetSeismicIntensity5Lower() {
+    public void inCaseOfValue3GetSeismicIntensity5Lower() {
         assertThat(Warning.SeismicIntensity5Lower, is(Warning.getSeismicIntensity(3)));
     }
     @Test
-    public void cangetSeismicIntensityUpper5() {
+    public void inCaseOfValue4GetSeismicIntensityUpper5() {
         assertThat(Warning.SeismicIntensityUpper5, is(Warning.getSeismicIntensity(4)));
     }
     @Test
-    public void cangetSeismicIntensity6Lower() {
+    public void inCaseOfValue5GetSeismicIntensity6Lower() {
         assertThat(Warning.SeismicIntensity6Lower, is(Warning.getSeismicIntensity(5)));
     }
     @Test
-    public void cangetSeismicIntensityUpper6() {
+    public void inCaseOfValue6GetSeismicIntensityUpper6() {
         assertThat(Warning.SeismicIntensityUpper6, is(Warning.getSeismicIntensity(6)));
     }
     @Test
-    public void cangetSeismicIntensity7() {
+    public void inCaseOfValue7GetSeismicIntensity7() {
         assertThat(Warning.SeismicIntensity7, is(Warning.getSeismicIntensity(7)));
     }
 }
